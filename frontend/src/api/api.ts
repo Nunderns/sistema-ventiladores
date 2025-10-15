07e0d78d69548145b1cad269efa9f7f8bbd6aa89
Henri Okayama <henri.okayama@gmail.com>
2025-10-15 13:03:22 -0300

    fix: correção de api com o front

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { InternalAxiosRequestConfig } from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "http://127.0.0.1:8000/api",
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
